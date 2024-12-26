a81de049e4540d578a0889243fba4a2d82a9259b
Erissa Renée Duvall <corgidev@outlook.com>
2024-12-25 21:03:32 -0500

    Initial file upload.

---
+++ decoder.ps1
@@ -0,0 +1,9 @@
+# TODO: Take in a code from the user
+
+# TODO: Make the provided string into an array with each character an item in the array.
+
+# TODO: Review each character in the array.
+
+# TODO: If a letter, swap it with its corresponding letter in the decoder and add it to a new array.
+
+# TODO: If not letter, just move it to the new array.
