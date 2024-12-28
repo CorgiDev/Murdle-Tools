5ae96213e93320546dafd80353726c5b5108b892
Erissa Renée Duvall <corgidev@outlook.com>
2024-12-27 21:17:34 -0500

    Updated script.

--- decoder.ps1
+++ decoder.ps1
@@ -16,6 +16,9 @@ function New-Decoder () {
 # Murdle Volume 1 Code
 $decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"
 
+# This only works if it is a Next Letter Code.
+# $decoder = "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","A"
+
 # Take in a code from the user and make it into an array.
 $scrambledPhrase = Read-Host -Prompt "Enter the code from your Murdle puzzle."
 $codeArray = $scrambledPhrase.ToCharArray()
