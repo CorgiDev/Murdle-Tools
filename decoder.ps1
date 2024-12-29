1abaa7d71ead5e4631be1294c949ff0ee80110de
Erissa Renée Duvall <corgidev@outlook.com>
2024-12-29 18:58:09 -0500

    Edited functions.

--- decoder.ps1
+++ decoder.ps1
@@ -2,6 +2,9 @@
 # Murdle Volume 1 Examples
 ## Example 1: TVMVIZO XLUUVV WRW MLG SZEV GSV XILDYZI
 ## Example 2: ZTVMG RMP SZW Z NVWRFN-DVRTSG DVZKLM
+# Murdle Volume 2 Examples
+# Murdle: The School of Mystery Exmples
+## Example 1: I II XV XV XI XXI XIX V IV XX XV II V
 
 ######################################################
 # FUNCTIONS
@@ -15,87 +18,98 @@ function Custom-Decoder () {
 # Used when it is letter to letter translation of 26 characters. Typically used with Alphabet-Translation function.
 function Simple-Prep () {
     $scrambledPhrase = Read-Host -Prompt "Enter the code from your Murdle puzzle.";
-    $preparedCodeArray = $scrambledPhrase.ToCharArray();
+    $scrambledPhraseUpper = $scrambledPhrase.ToUpper();
+    $preparedCodeArray = $scrambledPhraseUpper.ToCharArray();
     return $preparedCodeArray
 }
 
 function Numerals-Prep () {
-    Write-Host "YOU MUST group the numerals as shown in your puzzle for accurate translation."
+    Write-Host "YOU MUST group the numerals as shown in your puzzle for accurate translation, including spaces."
     Write-Host "Use the letter i, v, and x."
     Write-Host "Upper or lower case is not important."
     $scrambledNumerals = Read-Host -Prompt "Enter the numerals from your Murdle puzzle.";
-    $splitOption =  [System.StringSplitOptions]::None;
-    $preppedNumerals = $scrambledNumerals.Split(" ", $splitOption);
-    return $numeralsArray
+    $uppercaseNumerals = $scrambledNumerals.ToUpper();
+    $splitOption =  [System.StringSplitOptions]::RemoveEmptyEntries;
+    $preppedNumerals = $uppercaseNumerals.Split();
+    return $preppedNumerals
 }
 
-# Primarily used for Murdl The School of Mystery
-function Numerals-Translation ($numeralsArray, $decoder) {
+# Primarily used for Murdle The School of Mystery
+function Numerals-Translation () {
+    param (
+        [Array] $NumeralsArray,
+        [Array] $Decoder
+    )
+
     $numeralResult ="";
-    foreach ($numeralPiece in $numeralsArray) {
+    foreach ($numeralPiece in $NumeralsArray) {
         switch ($numeralPiece) {
-            I {$numeralResult += $decoder[0]}
-            II {$numeralResult += $decoder[1]}
-            III {$numeralResult += $decoder[2]}
-            IV {$numeralResult += $decoder[3]}
-            V {$numeralResult += $decoder[4]}
-            VI {$numeralResult += $decoder[5]}
-            VII {$numeralResult += $decoder[6]}
-            VIII {$numeralResult += $decoder[7]}
-            IX {$numeralResult += $decoder[8]}
-            X {$numeralResult += $decoder[9]}
-            XI {$numeralResult += $decoder[10]}
-            XII {$numeralResult += $decoder[11]}
-            XIII {$numeralResult += $decoder[12]}
-            XIV {$numeralResult += $decoder[13]}
-            XV {$numeralResult += $decoder[14]}
-            XVI {$numeralResult += $decoder[15]}
-            XVII {$numeralResult += $decoder[16]}
-            XVIII {$numeralResult += $decoder[17]}
-            XIX {$numeralResult += $decoder[18]}
-            XX {$numeralResult += $decoder[19]}
-            XXI {$numeralResult += $decoder[20]}
-            XXII {$numeralResult += $decoder[21]}
-            XXIII {$numeralResult += $decoder[22]}
-            XXIV {$numeralResult += $decoder[23]}
-            XXV {$numeralResult += $decoder[24]}
-            XXVI {$numeralResult += $decoder[25]}
-            Default {$numeralResult += $numeralPiece} 
+            "I" {$numeralResult += $Decoder[0]}
+            "II" {$numeralResult += $Decoder[1]}
+            "III" {$numeralResult += $Decoder[2]}
+            "IV" {$numeralResult += $Decoder[3]}
+            "V" {$numeralResult += $Decoder[4]}
+            "VI" {$numeralResult += $Decoder[5]}
+            "VII" {$numeralResult += $Decoder[6]}
+            "VIII" {$numeralResult += $Decoder[7]}
+            "IX" {$numeralResult += $Decoder[8]}
+            "X" {$numeralResult += $Decoder[9]}
+            "XI" {$numeralResult += $Decoder[10]}
+            "XII" {$numeralResult += $Decoder[11]}
+            "XIII" {$numeralResult += $Decoder[12]}
+            "XIV" {$numeralResult += $Decoder[13]}
+            "XV" {$numeralResult += $Decoder[14]}
+            "XVI" {$numeralResult += $Decoder[15]}
+            "XVII" {$numeralResult += $Decoder[16]}
+            "XVIII" {$numeralResult += $Decoder[17]}
+            "XIX" {$numeralResult += $Decoder[18]}
+            "XX" {$numeralResult += $Decoder[19]}
+            "XXI" {$numeralResult += $Decoder[20]}
+            "XXII" {$numeralResult += $Decoder[21]}
+            "XXIII" {$numeralResult += $Decoder[22]}
+            "XXIV" {$numeralResult += $Decoder[23]}
+            "XXV" {$numeralResult += $Decoder[24]}
+            "XXVI" {$numeralResult += $Decoder[25]}
+            Default {$numeralResult += $numeralPiece}
         }
     }
     return $numeralResult;
 }
 
-function Alphabet-Translation ($codeArray, $decoder) {
-    foreach ($codeChar in $codeArray) {
+function Alphabet-Translation () {
+    param (
+        [Array] $CodeArray,
+        [Array] $Decoder
+    )
+    foreach ($CodeChar in $CodeArray) {
         $alphabetResult="";
-        switch ($codeChar) {
-            A {$alphabetResult += $decoder[0]}
-            B {$alphabetResult += $decoder[1]}
-            C {$alphabetResult += $decoder[2]}
-            D {$alphabetResult += $decoder[3]}
-            E {$alphabetResult += $decoder[4]}
-            F {$alphabetResult += $decoder[5]}
-            G {$alphabetResult += $decoder[6]}
-            H {$alphabetResult += $decoder[7]}
-            I {$alphabetResult += $decoder[8]}
-            J {$alphabetResult += $decoder[9]}
-            K {$alphabetResult += $decoder[10]}
-            L {$alphabetResult += $decoder[11]}
-            M {$alphabetResult += $decoder[12]}
-            N {$alphabetResult += $decoder[13]}
-            O {$alphabetResult += $decoder[14]}
-            P {$alphabetResult += $decoder[15]}
-            Q {$alphabetResult += $decoder[16]}
-            R {$alphabetResult += $decoder[17]}
-            S {$alphabetResult += $decoder[18]}
-            T {$alphabetResult += $decoder[19]}
-            U {$alphabetResult += $decoder[20]}
-            V {$alphabetResult += $decoder[21]}
-            W {$alphabetResult += $decoder[22]}
-            X {$alphabetResult += $decoder[23]}
-            Y {$alphabetResult += $decoder[24]}
-            Z {$alphabetResult += $decoder[25]}
+        switch ($CodeChar) {
+            A {$alphabetResult += $Decoder[0]}
+            B {$alphabetResult += $Decoder[1]}
+            C {$alphabetResult += $Decoder[2]}
+            D {$alphabetResult += $Decoder[3]}
+            E {$alphabetResult += $Decoder[4]}
+            F {$alphabetResult += $Decoder[5]}
+            G {$alphabetResult += $Decoder[6]}
+            H {$alphabetResult += $Decoder[7]}
+            I {$alphabetResult += $Decoder[8]}
+            J {$alphabetResult += $Decoder[9]}
+            K {$alphabetResult += $Decoder[10]}
+            L {$alphabetResult += $Decoder[11]}
+            M {$alphabetResult += $Decoder[12]}
+            N {$alphabetResult += $Decoder[13]}
+            O {$alphabetResult += $Decoder[14]}
+            P {$alphabetResult += $Decoder[15]}
+            Q {$alphabetResult += $Decoder[16]}
+            R {$alphabetResult += $Decoder[17]}
+            S {$alphabetResult += $Decoder[18]}
+            T {$alphabetResult += $Decoder[19]}
+            U {$alphabetResult += $Decoder[20]}
+            V {$alphabetResult += $Decoder[21]}
+            W {$alphabetResult += $Decoder[22]}
+            X {$alphabetResult += $Decoder[23]}
+            Y {$alphabetResult += $Decoder[24]}
+            Z {$alphabetResult += $Decoder[25]}
             Default {$alphabetResult += $codeChar} 
         }
     }
@@ -113,10 +127,28 @@ function Book-Selection () {
     Switch ($selectBook) {
         1 {
             $decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"
+            $userCode = Simple-Prep
+            $result = Alphabet-Translation($userCode, $decoder)
+            Write-Host $result
         }
-        #2 {$decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"}
-        #3 {$decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"}
-        #4 {$decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"}
+        2 {
+            $decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"
+            $result = ""
+            Write-Host "This book has not yet been added to this script."
+            break
+        }
+        3 {
+            $decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"
+            $result = ""
+            Write-Host "This book has not yet been added to this script."
+            break
+        }
+        4 {
+            $decoder = "Z","Y","X","W","V","U","T","S","R","Q","P","O","N","M","L","K","J","I","H","G","F","E","D","C","B","A"
+            $result = ""
+            Write-Host "This book has not yet been added to this script."
+            break
+        }
         8 {
             $numeralsDecoder = "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
             $userNumerals = Numerals-Prep
@@ -125,7 +157,7 @@ function Book-Selection () {
         }
         Default {
             Write-Host "The number you entered does not appear to match an available option at this time.";
-            Write-Host "Please manually enter the code translation line. This will be the line opposite the regular ABCD line.";
+            Write-Host "Please manually enter the code translation line. This will typically be the line opposite the regular ABCD line.";
             $decoder = Custom-Decoder;
         }
     }
